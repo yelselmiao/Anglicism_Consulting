c3d696063e35960597153904ae96c94f3c6d44f2
Shuyi Tan <56212009+yelselmiao@users.noreply.github.com>
2022-02-26 17:29:03 -0800

    Update Cleaning.R

--- Code/Cleaning.R
+++ Code/Cleaning.R
@@ -1,53 +1,58 @@
 # This R script is used for pre-cleaning 
 
+pre_clean <- function(df) {
+  data <- raw_data %>%
+    dplyr::select(c(2:36, 53, 55, 57, 60, 61, 68)) %>%
+    rename(
+      p1_1 = Q4_1,
+      # part 1: 15 sentences
+      p1_2 = Q5_1,
+      p1_3 = Q6_1,
+      p1_4 = Q7_1,
+      p1_5 = Q8_1,
+      p1_6 = Q9_1,
+      p1_7 = Q10_1,
+      p1_8 = Q11_1,
+      p1_9 = Q12_1,
+      p1_10 = Q13_1,
+      p1_11 = Q14_1,
+      p1_12 = Q15_1,
+      p1_13 = Q16_1,
+      p1_14 = Q17_1,
+      p1_15 = Q18_1,
+      # part 2: 10 questions
+      p2_1_choice = Q20,
+      p2_1_other = Q20_3_TEXT,
+      p2_2_choice = Q21,
+      p2_2_other = Q21_3_TEXT,
+      p2_3_choice = Q22,
+      p2_3_other = Q22_3_TEXT,
+      p2_4_choice = Q23,
+      p2_4_other = Q23_3_TEXT,
+      p2_5_choice = Q24,
+      p2_5_other = Q24_3_TEXT,
+      p2_6_choice = Q25,
+      p2_6_other = Q25_3_TEXT,
+      p2_7_choice = Q26,
+      p2_7_other = Q26_3_TEXT,
+      p2_8_choice = Q27,
+      p2_8_other = Q27_3_TEXT,
+      p2_9_choice = Q28,
+      p2_9_other = Q28_3_TEXT,
+      p2_10_choice = Q29,
+      p2_10_other = Q29_3_TEXT,
+      gender = Q45,
+      age = Q46,
+      grew_up = `...57`,
+      live = `...60`,
+      edu_level = Q50,
+      eng_pro = Q51
+    ) %>%
+    dplyr::select(-c(seq(17, 35, 2))) %>%
+    mutate_at(c(1:15, 27), as.numeric) %>%
+    mutate_if(is.character, as.factor)
+  
+  return(data)
+}
 
-raw_data <- readxl::read_xlsx(paste0(here::here(), '/Data/survey_data.xlsx'), sheet = "Results")
-raw_data <- raw_data[2:(nrow(raw_data)-1),]
 
-data <- raw_data %>%
-  dplyr::select(c(2:36, 53, 55, 57, 60, 61, 68)) %>%
-  rename(p1_1 = Q4_1, # part 1: 15 sentences
-         p1_2 = Q5_1,
-         p1_3 = Q6_1,
-         p1_4 = Q7_1,
-         p1_5 = Q8_1,
-         p1_6 = Q9_1,
-         p1_7 = Q10_1,
-         p1_8 = Q11_1,
-         p1_9 = Q12_1,
-         p1_10 = Q13_1,
-         p1_11 = Q14_1,
-         p1_12 = Q15_1,
-         p1_13 = Q16_1,
-         p1_14 = Q17_1,
-         p1_15 = Q18_1, # part 2: 10 questions
-         p2_1_choice = Q20,
-         p2_1_other = Q20_3_TEXT,
-         p2_2_choice = Q21,
-         p2_2_other = Q21_3_TEXT,
-         p2_3_choice = Q22,
-         p2_3_other = Q22_3_TEXT,
-         p2_4_choice = Q23,
-         p2_4_other = Q23_3_TEXT,
-         p2_5_choice = Q24,
-         p2_5_other = Q24_3_TEXT,
-         p2_6_choice = Q25,
-         p2_6_other = Q25_3_TEXT,
-         p2_7_choice = Q26,
-         p2_7_other = Q26_3_TEXT,
-         p2_8_choice = Q27,
-         p2_8_other = Q27_3_TEXT,
-         p2_9_choice = Q28,
-         p2_9_other = Q28_3_TEXT,
-         p2_10_choice = Q29,
-         p2_10_other = Q29_3_TEXT,
-         gender = Q45, 
-         age = Q46, 
-         grew_up = `...57`, 
-         live = `...60`, 
-         edu_level = Q50, 
-         eng_pro = Q51
-  ) %>% 
-  dplyr::select(-c(seq(17, 35, 2))) %>%
-  mutate_at(c(1:15, 27), as.numeric) %>% 
-  mutate_if(is.character, as.factor)
